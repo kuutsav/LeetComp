747aa251f09a9d1481520141371a8fcbe0870637
kuutsav <krum.utsav@gmail.com>
2022-02-22 15:52:12 +0530

    added tests

---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+from leetcomp.utils import get_today
+
+
+def test_get_today():
+    today = get_today()
+    today_splits = today.split("_")
+    assert int(today_splits[0]) == 2022
+    assert int(today_splits[1]) in set(range(1, 13))
+    assert int(today_splits[2]) in set(range(1, 32))
