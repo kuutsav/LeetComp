908514c1b99c3f8aa2173f0668952d76421e3df9
utsav <krum.utsav@gmail.com>
2023-07-22 13:38:11 +0530

    updated a tes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,6 @@ from leetcomp.utils import get_today
 def test_get_today():
     today = get_today()
     today_splits = today.split("_")
-    assert int(today_splits[0]) == 2022
+    assert int(today_splits[0]) == 2023
     assert int(today_splits[1]) in set(range(1, 13))
     assert int(today_splits[2]) in set(range(1, 32))
